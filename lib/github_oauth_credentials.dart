6ee59887d062a65d4e9e59cc20fe26bdc8b488db
Richard Tan <khoonie@hotmail.com>
2022-11-04 19:38:14 +0800

    Initial Commit

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'a76597258159a69a9e58';
+const githubClientSecret = 'd499553c3b5bc48850bce766a73d1133fb3f1b2d';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
